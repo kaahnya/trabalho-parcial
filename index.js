db762d88b6e68548a1222cfc99d3cc4e0d27d09f
kaahnya <karina.decleva26@gmail.com>
2022-09-19 23:38:53 -0400

    Commit inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path');
+const app = express();
+const port = 3000
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.get('/formacao-academica', (req, res) => {
+  res.sendFile(path.join(__dirname, '/formacao-academica.html'));
+})
+
+app.get('/projetos', (req, res) => {
+  res.sendFile(path.join(__dirname, '/projetos.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
